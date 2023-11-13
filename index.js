9b93d7207104d011fe248ee04197e5fda3cf0de3
Assar Grimlund <assar.grimlund@gmail.com>
2023-11-13 21:12:40 +0100

    add bikes- and boat-house-pages

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ app.get('/', (req, res) => {
     res.render('home', { title: 'Välkommen till Svartsö Lanthandel' });
 })
 
+app.get('/gasthamn', (req, res) => {
+    res.render('guest-harbor', { title: 'Svartsö Gästhman Guest Port SeaStop Dockspot' });
+});
+
+app.get('/cykeluthyrning', (req, res) => {
+    res.render('bike-rental', { title: 'Cykeluthyrning - Svartsö Lanthandel AB' });
+});
+
+app.get('/sjobodarna', (req, res) => {
+    res.render('boat-houses', { title: 'Sjöbodarna - Svartsö Lanthandel AB' });
+});
+
+app.get('/om-svartso', (req, res) => {
+    res.render('about-us', { title: 'Om Svartsö - Svartsö Lanthandel AB' });
+});
+
 app.listen(3000, (req, res) => {
     console.log('Listening on port 3000.');
 })
