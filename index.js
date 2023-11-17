c1a69b609cc76d037b5f39b3695ed4e2053ffb19
Assar Grimlund <assar.grimlund@gmail.com>
2023-11-17 16:04:34 +0100

    start nodemailer

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const 
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, '/views'));
 
