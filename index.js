4808ccec151b8455fa584717052a6258f3177bad
Assar Grimlund <assar.grimlund@gmail.com>
2023-11-17 19:06:42 +0100

    add functionality to every form and make small change to navbar

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config();
 
-const 
+const { transporter } = require('./nodemailerConfig');
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, '/views'));
@@ -12,27 +13,64 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('home', { title: 'Välkommen till Svartsö Lanthandel' });
+    res.render('home', { title: 'Välkommen till Svartsö Lanthandel', page: 'home' });
 })
 
 app.get('/gasthamn', (req, res) => {
-    res.render('guest-harbor', { title: 'Svartsö Gästhman Guest Port SeaStop Dockspot' });
+    res.render('guest-harbor', { title: 'Svartsö Gästhman Guest Port SeaStop Dockspot', page: 'guest-harbor' });
 });
 
 app.get('/cykeluthyrning', (req, res) => {
-    res.render('bike-rental', { title: 'Cykeluthyrning - Svartsö Lanthandel AB' });
+    res.render('bike-rental', { title: 'Cykeluthyrning - Svartsö Lanthandel AB', page: 'bike-rental' });
 });
 
 app.get('/sjobodarna', (req, res) => {
-    res.render('boat-houses', { title: 'Sjöbodarna - Svartsö Lanthandel AB' });
+    res.render('boat-houses', { title: 'Sjöbodarna - Svartsö Lanthandel AB', page: 'boat-houses' });
 });
 
 app.get('/om-svartso', (req, res) => {
-    res.render('about-us', { title: 'Om Svartsö - Svartsö Lanthandel AB' });
+    res.render('about-us', { title: 'Om Svartsö - Svartsö Lanthandel AB', page: 'about-us' });
 });
 
 app.get('/kontakt', (req, res) => {
-    res.render('contact-us', { title: 'Kontakt - Svartsö Lanthandel AB' })
+    res.render('contact-us', { title: 'Kontakt - Svartsö Lanthandel AB', page: 'contact-us' })
+})
+
+app.post('/send-email', (req, res) => {
+    console.log(req.body);
+    const { name, phoneNumber, email, message } = req.body;
+    let dateBoatHouse = '';
+    let dateBoatHouseString = '';
+    try {
+        dateBoatHouse = req.body.date;
+        if(dateBoatHouse) {
+            dateBoatHouseString = "\nDatum: " + dateBoatHouse;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+    console.log(dateBoatHouseString);
+
+    const mailToAdmin = `Namn: ${name}\nTelefonnummer: ${phoneNumber}\nMailaddress: ${email}\nMeddelande: ${message}${dateBoatHouseString}`;
+
+    const mailOptions = {
+        from: process.env.EMAIL_ADDRESS,
+        to: process.env.EMAIL_ADDRESS,
+        subject: 'Testing Nodemailer',
+        text: mailToAdmin
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Form submission failed.' });
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).json({ success: 'Form submitted successfully!' })
+            res.redirect('/');
+        }
+    })
 })
 
 app.listen(3000, (req, res) => {
--- public/JS/app.js
+++ public/JS/app.js
@@ -1,5 +1,8 @@
 // Carousel
 // Manual navigation
+const slider = document.querySelector('.slider');
+if(slider) {
+
 const slides = document.querySelector('.slides');
 const firstSlide = document.querySelector('.slide.first');
 const carouselButtons = document.querySelectorAll('.manual-btn');
@@ -62,3 +65,49 @@ function scrollCarousel() {
 }
 
 let carouselTimer = setInterval(scrollCarousel, carouselInterval);
+
+}
+
+// Form functionality
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector('.contact-form');
+
+    if(form) {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+    
+            const formData = new FormData(form);
+            console.log(formData);
+
+            const formDataObj = {};
+            for (const [key, value] of formData) {
+                formDataObj[key] = value;
+            }
+            console.log(formDataObj);
+            
+            const formDataJSON = JSON.stringify(formDataObj);
+
+            fetch('/send-email', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: formDataJSON
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    console.log('The message was sent successfully!');
+                } else {
+                    console.error(data.error);
+                }
+            })
+            .catch(error => {
+                console.error('There was an error: ', error);
+            })
+
+            form.reset();
+        })
+    }
+})
+
